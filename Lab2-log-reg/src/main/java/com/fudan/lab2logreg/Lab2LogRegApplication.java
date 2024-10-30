3a96a8718d3a9eaba074bbac5fc64574465ea718
Wuxi <wu775851627@163.com>
2024-10-31 00:26:42 +0800

    new

---
+++ Lab2-log-reg/src/main/java/com/fudan/lab2logreg/Lab2LogRegApplication.java
@@ -0,0 +1,13 @@
+package com.fudan.lab2logreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2LogRegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2LogRegApplication.class, args);
+    }
+
+}
---
+++ Lab2-log-reg/src/test/java/com/fudan/lab2logreg/Lab2LogRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fudan.lab2logreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2LogRegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
